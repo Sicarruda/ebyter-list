453d116ab575bd606dcd3332780f9fefb8d7e918
Jessica Arruda <sicarruda@gmail.com>
2021-11-04 16:47:53 -0300

    Estabelecendo contato com o BD

--- back-end/index.js
+++ back-end/index.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const taskRoute = require('./modules/route')
 const app = express();
 const port = 3001;
+
+app.use('/home', taskRoute)
 
 app.listen(port, () => {
   console.log(`Server started on ${port}`);
